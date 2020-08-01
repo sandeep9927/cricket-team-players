fc5688095269dc5ead147c5849a757f214431c59
sandeep <sandeepsaini24398@gmail.com>
2020-08-01 18:19:31 +0530

    index.php edit

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body style="background-image: url('image/dhoni.jpg');">
-  <?php //include "navbar.php"?>
-  <div class="list-group" style="text-align: center; margin-left:25%; margin-right:25%;" >
-      <h3 style="text-align: center;" class="list-group-item list-group-item-action active">Select T    eam</h3>
-  
-  <a href="team.php" class="list-group-item list-group-item-action">Mumbai Champs.</a>
-  <a href="#" class="list-group-item list-group-item-action">Chennai Superstars</a>
-  <a href="#" class="list-group-item list-group-item-action">Chandigarh Lions.</a>
-  <a href="#" class="list-group-item list-group-item-action disabled">Hyderabad Heroes</a>
-  <a href="admin/login.php" class="btn btn-primary btn-lg btn-block">Admin Login</a>
-  
-</div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div>
+    <div style="height: 50px; width:500px;background-color:aqua;">
+      <h1 style="text-align: center;">Select Team</h1>
+    </div>
+    <div>
+      <table border="" style="height: 50px; width:500px;">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>image</th>
+            <th>Team_Name</th>
+            <th>City</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>4512.jpg</td>
+            <td>MI</td>
+            <td>rampur</td>
+            
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</body>
 </html>
-
